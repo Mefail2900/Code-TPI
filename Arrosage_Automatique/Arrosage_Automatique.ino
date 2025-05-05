f1872a30282cf0f447316899ae8e684663c30c63
Mefail2900 <mefailsule@gmail.com>
2025-05-05 09:54:48 +0200

    Create Arrosage_Automatique.ino

---
+++ Arrosage_Automatique/Arrosage_Automatique.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
