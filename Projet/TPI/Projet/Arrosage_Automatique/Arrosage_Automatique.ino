0aa01155867e41da1ba6695191ef783fc3c9ce97
Mefail2900 <mefailsule@gmail.com>
2025-05-09 16:34:12 +0200

    code Humidity avancé et rapport

--- Projet/TPI/Projet/Arrosage_Automatique/Arrosage_Automatique.ino
+++ Projet/TPI/Projet/Arrosage_Automatique/Arrosage_Automatique.ino
@@ -1,14 +1,21 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <DHT.h>
+int _moisture,sensor_analog;
+const int sensor_pin = A0;  /* Soil moisture sensor or you can put 35 for the GPIO*/
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(void){
+  Serial.begin(115200);     /* Set the baudrate to 115200*/
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void){
+  //Ask to the converter the analogic number 
+  sensor_analog = analogRead(sensor_pin);
+  
+  //4095 → 0 %, 0 → 100 %
+  _moisture = ( 100 - ( (sensor_analog/4095.00) * 100 ) );
 
+
+  Serial.print("Moisture = ");
+  Serial.print(_moisture);  /* Print Temperature on the serial window */
+  Serial.println("%");
+
+  delay(1000);              /* Wait for 1000mS */
 }
